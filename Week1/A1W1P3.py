6155cdb2a652e691ab580ceb851699c872d2e34a
StudyWithsharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-10 15:31:06 +0200

    Create A1W1P3.py

---
+++ Week1/A1W1P3.py
@@ -0,0 +1,7 @@
+#asking user for lenght and width and converting input to a float
+Length = float(input("What is the lenght? "))
+Width = float(input("What is the width? "))
+#processing: multiply Length and Widht
+Area = Length * Width
+#output: print a little message and the result of the multiplication 
+print("The area of the room is", Area, "square meters")
