0e0368f5615d561929ef2feae2a45d7683bf4557
StudyWithsharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-10 15:41:34 +0200

    Create A1W1P6.py

---
+++ Week1/A1W1P6.py
@@ -0,0 +1,10 @@
+#Input_user
+Days_input = int(input("Please enter the number of days "))
+
+#Proces
+Hours = Days_input * 24
+Minutes = Hours * 60 
+Seconds = Minutes * 60
+
+#Output 
+print(f"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}")
