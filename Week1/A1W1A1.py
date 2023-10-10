9f4e5ab1344559277957cac4c7ac978c4bec5bff
StudyWithsharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-10 15:05:03 +0200

    Update A1W1A1.py

--- Week1/A1W1A1.py
+++ Week1/A1W1A1.py
@@ -1,1 +1,9 @@
+#asking user input with input function and since user input is a string by default we use "int" to convert it to an integer so we can make calculations with the users input
+years = int(input("Enter the number of years: "))
 
+#creating new variables to save results to 
+months = years * 12
+days = years * 365
+
+#using an f-string to print result/output 
+print(f"Months:{months}, days: {days}")
