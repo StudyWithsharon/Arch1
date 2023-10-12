0b690c2120e1dc2d2b0e539465ceca2edd9e2433
StudyWithSharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-12 21:14:08 +0200

    Create Operators.py

---
+++ Practice/Operators.py
@@ -0,0 +1,62 @@
+"""
+There are many operator that you can use in programming. Operators can be categorised:
+
+1. Arithmetic Operators
+    +	Addition
+    -	Subtraction
+    *	Multiplication
+    /	Division
+    %	Modulus (Remainder)
+    ** Exponentiation
+    // Floor Division
+
+2. Comparison (Relational) Operators
+    ==	Equal
+    !=	Not Equal
+    >	Greater Than
+    <	Less Than
+    >= Greater Than or Equal
+    <= Less Than or Equal
+
+3. Logical Operators
+    and
+    or
+    not
+
+4. Assignment Operators
+    =	Assignment
+    +=	Addition Assignment
+    -=	Subtraction Assignment
+    *=	Multiplication Assignment
+    /=	Division Assignment
+    %=	Modulus Assignment
+    **=	Exponentiation Assignment
+    //=	Floor Division Assignment
+
+
+5. Bitwise Operators
+    &	Bitwise AND
+    |	Bitwise OR
+    ^	Bitwise XOR
+    ~	Bitwise NOT (Inversion)
+    <<	Left Shift
+    >>	Right Shift
+
+6. Membership Operators
+    in	True if value/variable is found in the sequence
+    not in	True if value/variable is not found in the sequence
+
+7. Identity Operators
+    is	True if both variables are the same object
+    is not	True if both variables are not the same object
+"""
+
+#Arithmetic Operators
+A = 9
+B = 9
+A + B = C
+print(C)
+#output --> 18
+
+
+
