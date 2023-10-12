2d2472e04b22214c0068fdf7e75769d04a2e2c6b
StudyWithSharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-12 20:25:35 +0200

    Create Lists.py

---
+++ Practice/Lists.py
@@ -0,0 +1,28 @@
+"""
+Lists are mutable objects in Python. They are created by using with [ ]. There are many ways to manipulate a list
+
+1. .append()
+2. .extend()
+3. .insert()
+4. .remove()
+5. .reverse()
+
+"""
+#let´s add an item to the list
+careers = ["painter", "fisherman", "policeman", "teacher"]
+careers.append("singer")
+#updated list--> ["painter", "fisherman", "policeman", "teacher", "singer"]
+
+#let´s add a list into another list
+toolbox = ["hammer", "saw", "screwdriver", "nails"]
+careers.append(toolbox)
+#updated list -->["painter", "fisherman", "policeman", "teacher", ["hammer", "saw", "screwdriver", "nails"]]
+
+
+numbers1 = [1, 2, 3, 4, 5, 6, 7]
+nubers2 = [8, 9, 10]
+
+
+
+fruits = ["orange", "cherry", "apple", "pear", "melon"]
+colors = ["red", "green", "blue", "brown", "yellow", "pink"]
