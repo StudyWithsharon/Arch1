ddb341da865b4051ae56121cb1efa258aca35101
StudyWithSharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-14 12:18:33 +0200

    Create Slicing.py

---
+++ Practice/Manipulations /Slicing.py
@@ -0,0 +1,11 @@
+#getting access to elements in a list
+
+#offset method
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sub_list = my_list[0]
+print(sub_list)
+
+#slicing
+a_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_list = a_list[1:5]
+print(new_list)
