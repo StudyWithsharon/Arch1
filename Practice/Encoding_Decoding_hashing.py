42809210a7d6766f033ceb40ca74aa3f195b40da
StudyWithSharon <147511594+StudyWithsharon@users.noreply.github.com>
2023-10-23 17:48:23 +0200

    Create Encoding_Decoding_hashing.py

---
+++ Practice/Encoding_Decoding_hashing.py
@@ -0,0 +1,9 @@
+#HASHING
+a = "Hello"
+print(hash(a))
+"""Hashing cannot be reversed back to original because it is a one-way operation"""
+
+#ENOCODING
+b = "Hello"
+b.encode(encoding='utf-8') #it will return a byte string
+"""Encoding is a reversible process"""
